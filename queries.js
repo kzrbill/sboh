8079fabf2d1c1d394b4827efb6e797d9e11fbbe2
Will Grange <will.grange@gmail.com>
2015-09-11 23:10:47 +0100

    Express API returning response

--- queries.js
+++ queries.js
@@ -5,36 +5,7 @@ var jsonfile = require('jsonfile');
 var file = 'sec.config.json'
 var config = jsonfile.readFileSync(file);
  
-
 var serviceBusService = azure.createServiceBusService(config.sbEndpoint);
-
-/**
-* Returns a list of topics.
-*
-* @param {object}             [options]                                 The request options.
-* @param {int}                [options.top]                             The number of topics to fetch.
-* @param {int}                [options.skip]                            The number of topics to skip.
-* @param {Function(error, listtopicsresult, response)} callback         `error` will contain information
-*                                                                       if an error occurs; otherwise `listtopicsresult` will contain
-*                                                                       the list of topics.
-*                                                                       `response` will contain information related to this operation.
-* @return {undefined}
-*/
-// ServiceBusService.prototype.listTopics = function (optionsOrCallback, callback) {
-//   this._listResources('/$Resources/Topics', topicResult, null, optionsOrCallback, callback);
-// };
-
-/*
-error: Error: 401 - Manage claim is required for this operation. TrackingId:9e3291d2-ff58-40e3-8185-97d3972d4654_G24,TimeStamp:9/11/2015 7:25:33 PM
-result: null
-response: [object Object]
-*/
-
-// 1. Create API with all current topics and subscriptions, and current messages in quques.
-// 2. Use these to handshake on sockets, to listen for message events by looking at 'monitoring' queues.
-
-
-
 
 function TopicsQuery()
 {
@@ -134,8 +105,6 @@ function OverviewQuery(dependencies)
 
                 callback(topics);
             });
-
-            
         });
     }
 }
@@ -171,7 +140,6 @@ function SubscriptionMapper()
 {
     this.fromRaw = function(rawSubscription)
     {
-        
         var subscription = new Subscription();
         subscription.name = rawSubscription.SubscriptionName;
         subscription.activeMessageCount = parseInt(rawSubscription.CountDetails['d2p1:ActiveMessageCount']);
@@ -180,14 +148,14 @@ function SubscriptionMapper()
     }
 }
 
-var dependencies = new Dependencies();
-var overviewQuery = new OverviewQuery(dependencies);
+// var dependencies = new Dependencies();
+// var overviewQuery = new OverviewQuery(dependencies);
 
-overviewQuery.get(function(overview){
-    console.log('overview');
-    console.log(JSON.stringify(overview, null, 4));
-});
+// overviewQuery.get(function(overview){
+//     console.log('overview');
+//     console.log(JSON.stringify(overview, null, 4));
+// });
 
-
-
+module.exports.Dependencies = Dependencies;
+module.exports.OverviewQuery = OverviewQuery;
 
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+var Dependencies = require('./queries').Dependencies;
+var OverviewQuery = require('./queries').OverviewQuery;
+
+app.get('/', function (req, res) {
+
+  var dependencies = new Dependencies();
+  var overviewQuery = new OverviewQuery(dependencies);
+
+  overviewQuery.get(function(overview){
+      res.json(overview);
+  });
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
